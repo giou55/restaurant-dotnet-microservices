85cc1d3d90e42d0a967bb5cb4af2d9b3cab0fea1
GiorgosG <giorgos.gioumertakis@bel.gr>
2024-05-06 19:01:07 +0300

    add coupon create

--- RestaurantApplication/Restaurant.Web/Controllers/CouponController.cs
+++ RestaurantApplication/Restaurant.Web/Controllers/CouponController.cs
@@ -30,5 +30,30 @@ namespace Restaurant.Web.Controllers
 
             return View(list);
         }
-    }
+
+		public async Task<IActionResult> CouponCreate()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CouponCreate(CouponDto model)
+		{
+			if (ModelState.IsValid)
+			{
+				ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+
+				if (response != null && response.IsSuccess)
+				{
+					TempData["success"] = "Coupon created successfully";
+					return RedirectToAction(nameof(CouponIndex));
+				}
+				else
+				{
+					TempData["error"] = response?.Message;
+				}
+			}
+			return View(model);
+		}
+	}
 }
