eef77ad376754d3c72a90f63618e29c9720dcea6
GiorgosG <giorgos.gioumertakis@bel.gr>
2024-05-07 18:22:45 +0300

    add DTOs for Authentication

---
+++ RestaurantApplication/Restaurant.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,42 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Restaurant.Services.AuthAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthAPIController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            //var errorMessage = await _authService.Register(model);
+            //if (!string.IsNullOrEmpty(errorMessage))
+            //{
+            //    _response.IsSuccess = false;
+            //    _response.Message = errorMessage;
+            //    return BadRequest(_response);
+            //}
+            //await _messageBus.PublishMessage(model.Email, _configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue"));
+            //return Ok(_response);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            //var loginResponse = await _authService.Login(model);
+            //if (loginResponse.User == null)
+            //{
+            //    _response.IsSuccess = false;
+            //    _response.Message = "Username or password is incorrect";
+            //    return BadRequest(_response);
+            //}
+            //_response.Result = loginResponse;
+            //return Ok(_response);
+            return Ok();
+        }
+    }
+}
---
+++ RestaurantApplication/Restaurant.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Services.AuthAPI.Models;
+
+namespace Restaurant.Services.AuthAPI.Data
+{
+    //public class AppDbContext : IdentityDbContext<IdentityUser>
+
+    // modify default IdentityUser and add a custom ApplicationUser with a new property
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
